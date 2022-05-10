1697ced6e4dfbf1789c40b2f9c78fd8eb335e7b0
Dewa Bagas <dewabagasql@gmail.com>
2022-05-10 10:06:33 +0700

    implements user update

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user.controller')
 const user = require('../middlewares/user');
+const middleware = require('../middlewares/auth')
 
 router.post('/register', user.validateUserRegister, controller.register);
 router.post('/login', user.validateUserLogin, controller.login);
+router.put('/:userId', middleware.verify,  controller.editUser);
 
 module.exports = router;
