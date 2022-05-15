c24f3f7964520558d90cbbb20baddc0ea3de4332
Hisyam-Dzaki <03hisyam@gmail.com>
2022-05-15 13:11:32 +0700

    Revision Unit testing comment-photo

--- tests/unit/photo.controller.test.js
+++ tests/unit/photo.controller.test.js
@@ -46,40 +46,31 @@ describe('PhotoController.addPhoto', () => {
 })
 
 describe('PhotoController.getPhoto', () => {
-    it('should return status code 201', async () => {
-        const user = {
-            id: 1,
-            photos: [{
-                id: 1,
-                poster_image_url: 'https://i.imgur.com/Xq2ZRXS.jpg',
-                title: 'Kita bermain',
-                caption: 'Kita bermain bersama teman-teman',
-                userid: 1
-            }]
+    it('should return status code 200', async () => {
+        const photo = {
+            poster_image_url: 'https://i.imgur.com/Xq2ZRXS.jpg',
+            title: 'Kita bermain',
+            caption: 'Kita bermain bersama teman-teman',
+            userid: 1
         };
-        const userModel = {
-            findOne: jest.fn(() => Promise.resolve(user))
+        const photoModel = {
+            findAll: jest.fn(() => Promise.resolve(photo))
         };
-        User.findOne = userModel.findOne
+        Photo.findAll = photoModel.findAll
         await PhotoController.getPhoto(req, res, next);
-        expect(res.statusCode) == (201)
-    })
-
+        expect(res.statusCode) == (200)
+    });
     it('should return status code 503', async () => {
-        const user = {
-            id: 1,
-            photos: [{
-                id: 1,
-                poster_image_url: 'https://i.imgur.com/Xq2ZRXS.jpg',
-                title: 'Kita bermain',
-                caption: 'Kita bermain bersama teman-teman',
-                userid: 1
-            }]
+        const photo = {
+            poster_image_url: 'https://i.imgur.com/Xq2ZRXS.jpg',
+            title: 'Kita bermain',
+            caption: 'Kita bermain bersama teman-teman',
+            userid: 1
         };
-        const userModel = {
-            findOne: jest.fn(() => Promise.reject(user))
+        const photoModel = {
+            findAll: jest.fn(() => Promise.reject(photo))
         };
-        User.findOne = userModel.findOne
+        Photo.findAll = photoModel.findAll
         await PhotoController.getPhoto(req, res, next);
         expect(res.statusCode) == (503)
     })
@@ -103,16 +94,30 @@ describe('PhotoController.editPhoto', () => {
         await PhotoController.editPhoto(req, res, next);
         expect(res.statusCode) == (201)
     })
-})
 
-describe('PhotoController.deletePhoto', () => {
-    it('should return status code 201', async () => {
+    it('should return status code 503', async () => {
         const photo = {
             id: 1,
             poster_image_url: 'https://i.imgur.com/Xq2ZRXS.jpg',
             title: 'Kita bermain',
             caption: 'Kita bermain bersama teman-teman',
             userid: 1
+        };
+        const photoModel = {
+            findOne: jest.fn(() => Promise.reject(photo)),
+            update: jest.fn(() => Promise.reject(photo))
+        };
+        Photo.findOne = photoModel.findOne
+        Photo.update = photoModel.update
+        await PhotoController.editPhoto(req, res, next);
+        expect(res.statusCode) == (503)
+    })
+})
+
+describe('PhotoController.deletePhoto', () => {
+    it('should return status code 201', async () => {
+        const photo = {
+            id: 1,
         };
         const photoModel = {
             findOne: jest.fn(() => Promise.resolve(photo)),
