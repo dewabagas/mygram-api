168cfd2c88262f8c27bf31fa595f75b4c1f74904
Dewa Bagas <dewabagasql@gmail.com>
2022-05-16 14:20:39 +0700

    configure dynamic prot

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('dotenv').config();
 const app = express()
+const port = process.env.PORT || 3000
+
 const userRoutes = require("./routes/user")
 const socialMediaRoutes = require("./routes/socialMedia")
 const photoRoutes = require("./routes/photo")
@@ -12,6 +14,6 @@ app.use('/socialmedias', socialMediaRoutes)
 app.use('/photos', photoRoutes)
 app.use('/comments', commentRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
